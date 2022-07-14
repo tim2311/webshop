7ec2accc8cd6887fb48299876f67bc78c6111669
tim <tim2311@hotmail.nl>
2022-07-14 18:13:27 +0200

    POST request data can be saved to database.

--- src/server/app.js
+++ src/server/app.js
@@ -2,19 +2,38 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 const bodyParser = require('body-parser');
-
-// Start Mongoose code.
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/customerDB");
+main().catch(err => console.log(err));
+
+mongoose.connect('mongodb://localhost:27017/customerDB');
 
 const customerSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-const Customer = new mongoose.model("Customer", customerSchema);
-// End Mongoose code.
+const Customer = mongoose.model('Customer', customerSchema);
+
+async function main() {
+    
+   
+    
+    // const newCustomer = new Customer({ email: 'user@email.com', password: '123' });
+    // console.log(newCustomer.name); // 'Silence'
+    // // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+    // customerSchema.methods.speak = function speak() {
+    //     const greeting = this.name
+    //         ? "Meow name is " + this.name
+    //         : "I don't have a name";
+    //     console.log(greeting);
+    // };
+  
+    // await newCustomer.save();
+    // const customers = await Customer.find();
+    // console.log(customers);
+
+}
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
@@ -34,6 +53,8 @@ app.post('/', function (req, res) {
 
 app.post('/signup', function (req, res) {
   console.log(req.body.email, req.body.password);
+  const newCustomer = new Customer({ email: req.body.email, password: req.body.password});
+  newCustomer.save();
 });
 
 app.listen(port, function () {
